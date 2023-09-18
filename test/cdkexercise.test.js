41a57a63d05eccddf6cc70acbbecaefe6c9aa3ad
kirstengammie <kirsten.gammie@zutari.com>
2023-09-18 15:20:49 +0100

    Initial commit

---
+++ test/cdkexercise.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Cdkexercise = require('../lib/cdkexercise-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdkexercise-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Cdkexercise.CdkexerciseStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
