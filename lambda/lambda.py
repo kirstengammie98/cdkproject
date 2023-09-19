124fc8e08782d90cbb5f3775a505d137840f8f36
kirstengammie <kirsten.gammie@zutari.com>
2023-09-19 14:16:25 +0100

    refactor lambda

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -2,23 +2,15 @@ import os
 import json
 import boto3
 
+TABLE_NAME = os.environ['DynamoDBTable']
+API_KEY = os.environ['API_KEY'] 
+
 s3_client = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
-table_name = os.environ['DynamoDBTable']
-api_key = os.environ['API_KEY'] 
+table = dynamodb.Table(TABLE_NAME)
 
 def lambda_handler(event, context):
     try:
-        # Check for the presence of the API Key in the request headers
-        api_key = event['headers']['x-api-key']
-
-        # Your API Key validation logic goes here
-        if api_key != api_key:
-            return {
-                'statusCode': 403,
-                'body': json.dumps('Access denied: Invalid API Key')
-            }
-        
         # Extract information from the S3 event
         bucket = event['Records'][0]['s3']['bucket']['name']
         object_key = event['Records'][0]['s3']['object']['key']
@@ -29,17 +21,25 @@ def lambda_handler(event, context):
         
         # Parse the file content (assuming it's JSON) and insert it into DynamoDB
         data = json.loads(file_content)
-        
-        # Get a reference to the DynamoDB table
-        table = dynamodb.Table(table_name)
+
+        # Check for the presence of the API Key in the request headers
+        key = event['headers']['x-api-key']
+
+        # Your API Key validation logic goes here
+        if key != API_KEY:
+            return {
+                'statusCode': 403,
+                'body': json.dumps('Access denied: Invalid API Key')
+            }
         
         # Insert data into DynamoDB
         table.put_item(Item=data)
-        
+
         return {
             'statusCode': 200,
             'body': json.dumps('Data inserted into DynamoDB successfully')
         }
+    
     except Exception as e:
         return {
             'statusCode': 500,
