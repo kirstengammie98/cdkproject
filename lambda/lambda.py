4a562bf8d9223deffa684285e90faf136921b4cc
kirstengammie <kirsten.gammie@zutari.com>
2023-09-19 17:22:12 +0100

    refactor for unique file id

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,38 +1,46 @@
-import os
 import json
 import boto3
+import os
+import uuid
+from botocore.exceptions import ClientError
 
-s3_bucket_name = os.environ['S3_BUCKET_NAME']
-table_name = os.environ['DYNAMODB_TABLE_NAME']
-
-s3_client = boto3.client('s3')
+s3 = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
-
+table_name = os.environ['DYNAMODB_TABLE_NAME']
+s3_bucket_name = os.environ['S3_BUCKET_NAME']
 
 def lambda_handler(event, context):
     try:
-        # Extract information from the S3 event
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        object_key = event['Records'][0]['s3']['object']['key']
+        # Generate a unique ID for the uploaded file
+        file_id = str(uuid.uuid4())
         
-        # Read the content of the file from S3
-        response = s3_client.get_object(Bucket=bucket, Key=object_key)
-        file_content = response['Body'].read().decode('utf-8')
+        # Retrieve the uploaded file from the request body
+        uploaded_file = event['body']  # Assuming the file content is in the request body
         
-        # Parse the file content (assuming it's JSON) and insert it into DynamoDB
-        data = json.loads(file_content)
-
+        # Generate a unique object key for the file
+        s3_object_key = f'{file_id}.json'
+        
+        # Upload the file content to S3
+        s3.put_object(Bucket=s3_bucket_name, Key=s3_object_key, Body=uploaded_file)
+        
+        # Insert data into DynamoDB, including the file ID and S3 object key
+        data = {
+            'id': file_id,
+            's3_object_key': s3_object_key,
+            'timestamp': str(event['requestContext']['requestTimeEpoch'])
+        }
+        
         table = dynamodb.Table(table_name)
-
+        
         # Insert data into DynamoDB
         table.put_item(Item=data)
-
+        
         return {
             'statusCode': 200,
-            'body': json.dumps('Data inserted into DynamoDB successfully')
+            'body': json.dumps('File uploaded and data inserted into DynamoDB successfully')
         }
     
-    except Exception as e:
+    except ClientError as e:
         return {
             'statusCode': 500,
             'body': json.dumps(f'Error: {str(e)}')
