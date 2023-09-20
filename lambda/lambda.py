e79e20125ae47d8d6d760e711862f0318e19bbce
kirstengammie <kirsten.gammie@zutari.com>
2023-09-20 09:45:26 +0100

    lambda fix

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -2,25 +2,26 @@ import os
 import json
 import boto3
 
-s3_bucket_name = os.environ['S3_BUCKET_NAME']
-table_name = os.environ['DYNAMODB_TABLE_NAME']
-
+table_name = os.environ['TABLE_NAME']
 s3_client = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
     try:
         # Extract information from the S3 event
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        object_key = event['Records'][0]['s3']['object']['key']
+        bucket_name = os.environ['BUCKET_NAME']
+        file_key = event['fileKey']
+
+        # bucket = event['Records'][0]['s3']['bucket']['name']
+        # object_key = event['Records'][0]['s3']['object']['key']
         
         # Read the content of the file from S3
-        response = s3_client.get_object(Bucket=bucket, Key=object_key)
+        response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
         file_content = response['Body'].read().decode('utf-8')
         
         # Parse the file content (assuming it's JSON) and insert it into DynamoDB
         data = json.loads(file_content)
-        
+
         table = dynamodb.Table(table_name)
 
         # Insert data into DynamoDB
