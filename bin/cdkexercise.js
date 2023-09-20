9091fc619a6ca61c6e151e20fab91d5b64fed351
kirstengammie <kirsten.gammie@zutari.com>
2023-09-20 16:04:36 +0100

    update to cdk v2

--- bin/cdkexercise.js
+++ bin/cdkexercise.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const cdk = require('aws-cdk-lib');
+const cdk = require('aws-cdk');
 const { CdkexerciseStack } = require('../lib/cdkexercise-stack');
 
 const app = new cdk.App();
